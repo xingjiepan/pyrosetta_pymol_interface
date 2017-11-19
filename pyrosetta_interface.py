7adf4fba40a2e0d8c3f0ec9d3ff40fbc57b49653
xingjiepan <xingjiepan@gmail.com>
2017-11-18 18:53:58 -0800

    Add the repack_selected_residues() function

--- pyrosetta_interface.py
+++ pyrosetta_interface.py
@@ -35,14 +35,43 @@ def color_pose_by_energy(name):
         cmd.set_color('res{0}_color'.format(i + 1), [ (e - emin) / (emax - emin), 0, (emax - e) / (emax - emin) ])
         cmd.color('res{0}_color'.format(i + 1), 'res {0}'.format(i + 1))
 
+def selection_to_residue_selector(pose_name, selection):
+    '''Get a residue selector corresponding to a pymol selection.'''
+    myspace = {'l': set()}
+    cmd.iterate(selection ,'l.add((model, resi + chain))', space=myspace)
+ 
+    return rosetta.core.select.residue_selector.ResidueIndexSelector(
+            ','.join(r[1] for r in myspace['l'] if r[0] == pose_name))
+
+def repack_selected_residues(pose_name, selection):
+    '''Repack the selected residues.'''
+    res_selector = selection_to_residue_selector(pose_name, selection)
+    not_res_selector = selection_to_residue_selector(pose_name, 'not ({0})'.format(selection))
+   
+    task_repack = rosetta.core.pack.task.operation.OperateOnResidueSubset(
+            rosetta.core.pack.task.operation.RestrictToRepackingRLT(), res_selector)
+    task_not_repack = rosetta.core.pack.task.operation.OperateOnResidueSubset(
+            rosetta.core.pack.task.operation.PreventRepackingRLT(), res_selector)
+
+    task_factory = rosetta.core.pack.task.TaskFactory()
+    task_factory.push_back(task_repack)
+    task_factory.push_back(task_not_repack)
+
+    packer = rosetta.protocols.simple_moves.PackRotamersMover()
+    packer.task_factory(task_factory)
+    packer.apply(poses[pose_name])
 
 cmd.extend("load_pose", load_pose)
 cmd.extend("score_pose", score_pose)
 cmd.extend("color_pose_by_energy", color_pose_by_energy)
+cmd.extend("repack_selected_residues", repack_selected_residues)
 
 
 ###TEST
-#load_pose('foo', 'demo/1arb.pdb')
+load_pose('foo', 'demo/1arb.pdb')
+
+#selection_to_residue_selector('(all)')
+#repack_selected_residues('(all)')
 
 #score_pose('foo')
 #color_pose_by_energy('foo')
